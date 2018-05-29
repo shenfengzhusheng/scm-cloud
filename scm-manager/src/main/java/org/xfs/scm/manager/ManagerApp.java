eff996c8f4c27333ca30c9ce10561937748c0658
liujincai <xixingyingzhongdui@gmail.com>
2018-05-29 23:25:34 +0800

    rework

---
+++ scm-manager/src/main/java/org/xfs/scm/manager/ManagerApp.java
@@ -0,0 +1,29 @@
+package org.xfs.scm.manager;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
+public class ManagerApp {
+
+    public static void main(String[]args){
+        SpringApplication.run(ManagerApp.class);
+    }
+
+
+}
+
+
---
+++ scm-manager/src/main/java/org/xfs/scm/manager/index/web/IndexWeb.java
@@ -0,0 +1,29 @@
+package org.xfs.scm.manager.index.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@Controller
+public class IndexWeb {
+
+
+    // inject via application.properties
+    @Value("${welcome.message:test}")
+    private String message = "Hello World";
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "welcome";
+    }
+    @ResponseBody
+    @RequestMapping("/weclome")
+    public Object index(){
+        return "hello world!";
+    }
+}
